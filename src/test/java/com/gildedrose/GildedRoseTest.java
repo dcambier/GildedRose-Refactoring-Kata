041405c02e274d9ccfd5d03bd54b0d8e76ade773
David Cambier <david.cambier@emkg.com.br>
2019-02-20 13:54:38 +0100

    Enhance tests + comments Rules

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,7 +6,15 @@ import org.junit.Test;
 
 public class GildedRoseTest {
 
-    //Once the sell by date has passed, Quality degrades twice as fast
+	//Rule #0 : At the end of each day our system lowers both values for every item
+    @Test
+    public void sellEndDay() {
+        Item[] items = new Item[] { new Item("Other Item", 2, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(app.items[0].sellIn,1);
+    }
+    //Rule #1 : Once the sell by date has passed, Quality degrades twice as fast
     @Test
     public void sellDatePassed() {
         Item[] items = new Item[] { new Item("Other Item", -1, 10) };
@@ -14,7 +22,7 @@ public class GildedRoseTest {
         app.updateQuality();
         assertEquals(app.items[0].quality,8);
     }
-    //The Quality of an item is never negative
+    //Rule #2 : The Quality of an item is never negative
     @Test
     public void qualityNeverNegative() {
         Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", -1, -1) };
@@ -22,7 +30,7 @@ public class GildedRoseTest {
         app.updateQuality();
         assertEquals(app.items[0].quality,0);
     }
-    //"Aged Brie" actually increases in Quality the older it gets
+    //Rule #3 : "Aged Brie" actually increases in Quality the older it gets
     @Test
     public void qualityAgedBrie() {
         Item[] items = new Item[] { new Item("Aged Brie", 2, 43) };
@@ -30,7 +38,7 @@ public class GildedRoseTest {
         app.updateQuality();
         assertEquals(app.items[0].quality,44);
     }
-    //The Quality of an item is never more than 50
+    //Rule #4 : The Quality of an item is never more than 50
     @Test
     public void qualityNeverMore50() {
         Item[] items = new Item[] { new Item("Aged Brie", 5, 51) };
@@ -38,7 +46,7 @@ public class GildedRoseTest {
         app.updateQuality();
         assertEquals(app.items[0].quality,50);
     }
-    //"Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+    //Rule #5 : "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
     @Test
     public void sulfurasQuality() {
         Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 10, 80) };
@@ -54,9 +62,11 @@ public class GildedRoseTest {
         assertTrue(app.items[0].sellIn == 10);
     }
     /*
-     * Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
-     * Quality drops to 0 after the concert
-     */
+	 * N/A ****  Rule #6 : "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
+	 * 			 Quality increases by 2 when there are 10 days or less 
+	 * 			 and by 3 when there are 5 days or less 
+	 * 			 but Quality drops to 0 after the concert
+	 */
     @Test
     public void qualityBackstage10() {
         Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20) };
@@ -71,6 +81,9 @@ public class GildedRoseTest {
         app.updateQuality();
         assertEquals(app.items[0].quality,23);
     }
+    /*
+     * Rule #"Conjured" items degrade in Quality twice as fast as normal items
+     */
     @Test
     public void qualityConjured() {
         Item[] items = new Item[] { new Item("Conjured", 5, 20) };
